d01aeaf17b93b14eb409cdc66dd9c38555dc6d99
j0x00 <admin@jnull.de>
2021-05-13 13:53:33 +0200

    updated pid config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -490,9 +490,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  #define DEFAULT_Kp 22.20
+  #define DEFAULT_Ki 1.44
+  #define DEFAULT_Kd 85.56
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
