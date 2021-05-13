70e3f989312369a91a08f8ff5283db802e51681c
j0x00 <admin@jnull.de>
2021-05-13 13:39:41 +0200

    updated PID config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -510,9 +510,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  16.8068
-    #define DEFAULT_Ki   0.9770
-    #define DEFAULT_Kd  72.2775
+    #define DEFAULT_Kp  16.6158
+    #define DEFAULT_Ki   0.9365
+    #define DEFAULT_Kd  73.7014
   #endif
 #endif // PIDTEMP
 
